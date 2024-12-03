3697723652877fe195f49441cdddbf4440933a0f
MarioAlonsoAlonso <112715624+MarioAlonsoAlonso@users.noreply.github.com>
2024-12-03 19:38:07 +0100

    Update deteccion_circulos.py

--- src/deteccion_circulos.py
+++ src/deteccion_circulos.py
@@ -15,11 +15,11 @@ circulos = cv2.HoughCircles(
     imagen_gris,
     cv2.HOUGH_GRADIENT,
     dp=1,
-    minDist=20,
+    minDist=100,
     param1=50,
     param2=30,
-    minRadius=10,
-    maxRadius=100
+    minRadius=1,
+    maxRadius=10000
 )
 
 # Verificar si se detectaron círculos
@@ -27,3 +27,15 @@ if circulos is not None:
     print("Se ha detectado al menos un círculo.")
 else:
     print("No se detectaron círculos.")
+
+if circulos is not None:
+    circulos = np.uint16(np.around(circulos))  # Redondear y convertir a entero
+    for i in circulos[0, :]:
+        # Dibujar el círculo
+        cv2.circle(imagen, (i[0], i[1]), i[2], (0, 255, 0), 2)
+        # Dibujar el centro del círculo
+        cv2.circle(imagen, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+cv2.imshow('Círculos detectados', imagen)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
